04acc7cb524faf41373a0bb3c4222d54729c0a16
Travis Smalling <main@smalling.dev>
2023-06-15 18:58:33 -0500

    initial commit
    can track + draw hand landmarks on screen, as well as functionality to display a letter to the screen

---
+++ main.py
@@ -0,0 +1,24 @@
+import hand_tracking_module as htm
+import cv2
+import mediapipe as mp
+import random
+
+cap = cv2.VideoCapture(0)
+tracker = htm.handTracker()
+
+def letter_parser():
+    letter = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    return f'{letter}'
+
+while True:
+    success, image = cap.read()
+    image = tracker.hands_finder(image)
+    lmList = tracker.position_finder(image)
+    tracker.letter_display(image, letter_parser())
+    #if len(lmList) != 0:
+        #print(lmList[4])
+
+    cv2.imshow("Video",image)
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+ 
