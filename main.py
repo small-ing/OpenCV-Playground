c34354df43ac5bbda227f71f9b17f9f8ff4f768b
Allen <you@example.com>
2023-06-19 13:37:33 -0400

    hello from allen

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ tracker = htm.handTracker()
 def letter_parser():
     letter = random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     return letter
-# hello
+# hello from allen
 # main loop
 while True:
     success, image = cap.read()
