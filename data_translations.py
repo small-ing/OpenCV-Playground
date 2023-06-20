83898340bfd245e44c04f6f90ea8b1cf2bcd0028
Travis Smalling <main@smalling.dev>
2023-06-20 13:35:09 -0500

    changed order of arguments in table
    [letter][joint][index_of_file][x/y]

--- data_translations.py
+++ data_translations.py
@@ -6,7 +6,7 @@ import numpy as np
 landmark_map = [None, 0, None, 5, 6, 7, 8, None, 9, 10, 11, 12, None, 17, 18, 19, 20, None, 13, 14, 15, 16, 1, 2, 3, 4]
 
 def collect_data(batch_size=10, offset=0):
-    empty_marks = np.zeros((21, 26, batch_size, 2))
+    empty_marks = np.zeros((26, 21, batch_size, 2))
     for index in range(offset, offset+batch_size):
         print
         zero_fill = ""
@@ -23,23 +23,26 @@ def collect_data(batch_size=10, offset=0):
             try:
                 with open(os.path.join("data", letter + "_annotation", zero_fill + str(index) + ".json")) as file:
                     data = json.load(file)
+                    
                     try:
                         assert letter == data["Letter"]
                     except AssertionError:
                         print("Letter mismatch: " + letter + " != " + data["Letter"] + "in File " + os.path.join("data", letter + "_annotation", zero_fill + str(index) + ".json"))
+                    
                     for joint in range(26): # splits original joints
                         for dim in range(2): # splits x/y
                             if landmark_map[joint] != None: # checks if joint is in new landmark map
-                                if empty_marks[landmark_map[joint]]["abcdefghijklmnopqrstuvwxyz".index(letter)][index-offset][dim] == 0:
-                                    empty_marks[landmark_map[joint]]["abcdefghijklmnopqrstuvwxyz".index(letter)][index-offset][dim] = data["Landmarks"][joint][dim]  
+                                if empty_marks["abcdefghijklmnopqrstuvwxyz".index(letter)][landmark_map[joint]][index-offset][dim] == 0:
+                                    empty_marks["abcdefghijklmnopqrstuvwxyz".index(letter)][landmark_map[joint]][index-offset][dim] = data["Landmarks"][joint][dim]  
                                 else: # break everything if there is already a value
                                     assert 1 + 1 == 3
+            
             except FileNotFoundError:
                 print("File not found: " + os.path.join("data", letter + "_annotation", zero_fill + str(index) + ".json"))
     return empty_marks
 
 def main():
-    print(collect_data(5, 500))
+    print(len(collect_data(1, 951)[10][:]))
 
 
 
