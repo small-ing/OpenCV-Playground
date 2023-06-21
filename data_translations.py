3e83ce1e7e4d6c06c2c11332263f29ec7b2fe87c
Travis Smalling <main@smalling.dev>
2023-06-21 11:10:25 -0500

    rebuilt the CNN
    working version using new CNN structure to improve debugging and showing better accuracy already

--- data_translations.py
+++ data_translations.py
@@ -4,6 +4,10 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.autograd import Variable
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import torch.nn.init
 import matplotlib.pyplot as plt
 
 
@@ -67,25 +71,67 @@ def normalize_data(data):
             data[i][j][1] = data[i][j][1] / 480 
     return data
 
-def create_model():
-    m = nn.Sequential(
-        nn.Conv2d(1, 18, (1, 2), padding=1),
-        nn.ReLU(),
-        nn.Conv2d(18, 162, (1, 2), padding=1),
-        nn.ReLU(),
-        nn.MaxPool2d((3, 3)),
+class CNN(torch.nn.Module):
 
-        nn.Conv2d(162, 162, (1, 2), padding=1),
-        nn.ReLU(),
-        nn.MaxPool2d((2, 2)),
+    def __init__(self):
+        super(CNN, self).__init__()
+        keep_prob = 0.5
+        # L1 ImgIn shape=(?, 21, 2, 1)
+        # Conv -> (?, 21, 2, 32)
+        # Pool -> (?, 10, 1, 32)
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, kernel_size=2, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.Dropout(p=1 - keep_prob))
+        # L2 ImgIn shape=(?, 10, 1, 32)
+        # Conv      ->(?, 10, 1, 64)
+        # Pool      ->(?, 7, 7, 64)
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv2d(32, 128, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            #torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.Dropout(p=1 - keep_prob))
+        # L3 ImgIn shape=(?, 7, 7, 64)
+        # Conv ->(?, 7, 7, 128)
+        # Pool ->(?, 4, 4, 128)
+        self.layer3 = torch.nn.Sequential(
+            torch.nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            #torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
+            torch.nn.Dropout(p=1 - keep_prob))
 
-        nn.Flatten(),
-        nn.Linear(810, 130),
-        nn.ReLU(),
-        nn.Linear(130, 26)
-    )
-    print(m)
-    return m
+        # L4 FC 4x4x128 inputs -> 625 outputs
+        self.fc1 = torch.nn.Linear(1408, 625, bias=True)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        self.layer4 = torch.nn.Sequential(
+            self.fc1,
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=1 - keep_prob))
+        # L5 Final FC 625 inputs -> 10 outputs
+        self.fc2 = torch.nn.Linear(625, 26, bias=True)
+        torch.nn.init.xavier_uniform_(self.fc2.weight) # initialize parameters
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.view(out.size(0), -1)   # Flatten them for FC
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+# def create_model():
+#     m = nn.Sequential(
+#             torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+#             torch.nn.ReLU(),
+#             torch.nn.MaxPool2d(kernel_size=2, stride=2),
+#             torch.nn.Dropout(p=1)
+    
+    
+#     )
+#     print(m)
+#     return m
 
 class ImageDataset(torch.utils.data.Dataset):
     def __init__(self, img, label):
@@ -97,6 +143,7 @@ class ImageDataset(torch.utils.data.Dataset):
         return self.img[idx], self.label[idx]
 
 def train_model(model, train_loader, loss_fn, optimizer, epochs, test_images, test_labels):
+    should_save = False
     for i in range(epochs):
         for img, label in train_loader:
             #print(img.shape)
@@ -121,7 +168,10 @@ def train_model(model, train_loader, loss_fn, optimizer, epochs, test_images, te
         # print(test_labels)
         test_labels = test_labels.to(device)
         acc = torch.sum(digit == test_labels)/len(test_labels)
+        if acc > 0.2:
+            should_save = True
         print(f"Epoch {i+1}: loss: {loss}, test accuracy: {acc}")
+    return should_save
 
 def test_model(model, test_images, test_labels):
     test_images = test_images.to(torch.float).to(device)
@@ -132,8 +182,8 @@ def test_model(model, test_images, test_labels):
     print(f"Test accuracy: {acc}")
 
 def main():
-    train_data, train_labels = collect_data(23400)
-    test_data, test_labels = collect_data(2600, 23400)
+    train_data, train_labels = collect_data(2340)
+    test_data, test_labels = collect_data(260, 2340)
 
     train_data = torch.from_numpy(train_data)
     train_data = normalize_data(train_data)
@@ -141,22 +191,27 @@ def main():
     test_data = normalize_data(test_data)
     train_labels = torch.from_numpy(train_labels)
     test_labels = torch.from_numpy(test_labels)
-
+    print(train_data.shape)
+    print(train_labels.shape)
     train_data = train_data.reshape(-1, 1, 21, 2)
+    print(train_data.shape)
     test_data = test_data.reshape(-1, 1, 21, 2)
     
+    train_data = train_data.float()
     train_dataset = ImageDataset(train_data, train_labels)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=130, shuffle=True, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=130, shuffle=True, num_workers=1)
 
-    model = create_model()
-    optimizer = optim.Adam(model.parameters(), lr=1e-5)
+    model = CNN()
+
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
     criteron = nn.CrossEntropyLoss()
 
     model.to(device)
-    train_model(model, train_loader, criteron, optimizer, 10, test_data, test_labels)
+    train_model(model, train_loader, criteron, optimizer, 8, test_data, test_labels)
+        # torch.save(model, "model.pth")
+        # torch.load("model.pth")
 
-    for i in range(5):
-        test_model(model, test_data, test_labels)
 
 
 
