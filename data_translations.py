d4153e4934ce5932fd37bf5ab9aeedf121961b4f
Travis Smalling <main@smalling.dev>
2023-06-20 23:22:16 -0500

    technically works
    definitely not good, but technically works

--- data_translations.py
+++ data_translations.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 #                0    1    2   3  4  5  6    7   8  9   10  11   12   13  14  15  16   17   18  19  20  21  22 23 24 25
 landmark_map = [None, 0, None, 5, 6, 7, 8, None, 9, 10, 11, 12, None, 17, 18, 19, 20, None, 13, 14, 15, 16, 1, 2, 3, 4]
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def collect_data(batch_size=26, offset=0):
     empty_marks = np.zeros((batch_size, 21, 2))
@@ -17,13 +18,13 @@ def collect_data(batch_size=26, offset=0):
     letter_batch = batch_size / 26
     letter_batch = int(letter_batch)
 
-    for letter in "abcdefghijklmnopqrstuvwxyz":
-        print("starting " + letter  + " batch")
+    for letter in alphabet:
+        #print("starting " + letter  + " batch")
         offset = offset // 26
         for index in range(offset, offset+letter_batch):
-            print("starting " + letter + " " + str(index))
-            total_index = index + "abcdefghijklmnopqrstuvwxyz".index(letter)*letter_batch
-            empty_labels[total_index-offset] = "abcdefghijklmnopqrstuvwxyz".index(letter)
+            #print("starting " + letter + " " + str(index))
+            total_index = index + alphabet.index(letter)*letter_batch
+            empty_labels[total_index-offset] = alphabet.index(letter)
             
             zero_fill = ""
             if index < 10:
@@ -57,7 +58,7 @@ def collect_data(batch_size=26, offset=0):
 
             except FileNotFoundError:
                 print("File not found: " + os.path.join("data", letter + "_annotation", zero_fill + str(index) + ".json"))
-    return empty_marks, empty_labels
+    return empty_marks, empty_labels.astype(int)
 
 def normalize_data(data):
     for i in range(len(data)):
@@ -68,22 +69,20 @@ def normalize_data(data):
 
 def create_model():
     m = nn.Sequential(
-    nn.Conv2d(1, 28, (3, 3), padding=1),
-      nn.ReLU(),
-    #   nn.MaxPool2d((2, 2)),
+        nn.Conv2d(1, 18, (1, 2), padding=1),
+        nn.ReLU(),
+        nn.Conv2d(18, 162, (1, 2), padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d((3, 3)),
 
-      nn.Conv2d(28, 56, (3, 3), padding=1),
-      nn.ReLU(),
-    #   nn.MaxPool2d((2, 2)),
+        nn.Conv2d(162, 162, (1, 2), padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d((2, 2)),
 
-      nn.Conv2d(56, 56, (3, 3), padding=1),
-      nn.ReLU(),
-    #   nn.MaxPool2d((2, 2)),
-
-      nn.Flatten(),
-      nn.Linear(504, 64),
-      nn.ReLU(),
-      nn.Linear(64, 26)
+        nn.Flatten(),
+        nn.Linear(810, 130),
+        nn.ReLU(),
+        nn.Linear(130, 26)
     )
     print(m)
     return m
@@ -100,30 +99,53 @@ class ImageDataset(torch.utils.data.Dataset):
 def train_model(model, train_loader, loss_fn, optimizer, epochs, test_images, test_labels):
     for i in range(epochs):
         for img, label in train_loader:
+            #print(img.shape)
+            #print(label.shape)
             img = img.to(device)
             img = img.to(torch.float)
             label = label.to(device)
-
-            pred = model(img)
             
+            pred = model(img)
+            #print("Prediction: " + str(pred))
+            #print("Actual: " + str(label))
+
             loss = loss_fn(pred, label)
+            #print(loss)
+
             optimizer.zero_grad()    
             loss.backward()
             optimizer.step()
         test_images = test_images.to(torch.float).to(device)
         pred = model(test_images)
         digit = torch.argmax(pred, dim=1)
+        # print(test_labels)
         test_labels = test_labels.to(device)
         acc = torch.sum(digit == test_labels)/len(test_labels)
         print(f"Epoch {i+1}: loss: {loss}, test accuracy: {acc}")
-    
+
+def test_model(model, test_images, test_labels):
+    test_images = test_images.to(torch.float).to(device)
+    pred = model(test_images)
+    digit = torch.argmax(pred, dim=1)
+    test_labels = test_labels.to(device)
+    acc = torch.sum(digit == test_labels)/len(test_labels)
+    print(f"Test accuracy: {acc}")
+
 def main():
     train_data, train_labels = collect_data(23400)
-    train_data = normalize_data(train_data)
-    train_data = torch.from_numpy(train_data)
-    train_dataset = ImageDataset(train_data, train_labels)
     test_data, test_labels = collect_data(2600, 23400)
+
+    train_data = torch.from_numpy(train_data)
+    train_data = normalize_data(train_data)
+    test_data = torch.from_numpy(test_data)
     test_data = normalize_data(test_data)
+    train_labels = torch.from_numpy(train_labels)
+    test_labels = torch.from_numpy(test_labels)
+
+    train_data = train_data.reshape(-1, 1, 21, 2)
+    test_data = test_data.reshape(-1, 1, 21, 2)
+    
+    train_dataset = ImageDataset(train_data, train_labels)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=130, shuffle=True, num_workers=2)
 
     model = create_model()
@@ -132,6 +154,11 @@ def main():
 
     model.to(device)
     train_model(model, train_loader, criteron, optimizer, 10, test_data, test_labels)
+
+    for i in range(5):
+        test_model(model, test_data, test_labels)
+
+
 
 if __name__ == "__main__":
     main()
