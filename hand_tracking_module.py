17c6bef1e74c36539335ae4bd46a5976d66c12b6
Travis Smalling <main@smalling.dev>
2023-06-21 10:28:26 -0500

    added tensor to store live locations

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -2,6 +2,8 @@
     #Programa visual de identificacion de manops al aire libre
 import cv2
 import mediapipe as mp
+from data_translations import *
+import torch
 
 class handTracker():
     def __init__(self, mode=False, maxHands=1, detectionCon=0.5,modelComplexity=1,trackCon=0.5):
@@ -19,6 +21,7 @@ class handTracker():
         self.idSelY_0 = 0
         self.stringOut_20 = ""
         self.stringOut_0 = ""
+        self.landmark_tensor = torch.zeros(1, 21, 2)
         
     def hands_finder(self,image,draw=True):
         imageRGB = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
@@ -39,6 +42,9 @@ class handTracker():
                 h,w,c = image.shape
                 cx,cy = int(lm.x*w), int(lm.y*h)
                 lmlist.append([id,cx,cy])
+                self.landmark_tensor[0][id][0] = cx
+                self.landmark_tensor[0][id][1] = cy
+                
             if id == 20 :
                    self.idSelX_20 = cx
                    self.idSelY_20 = cy
@@ -61,11 +67,16 @@ class handTracker():
         # Using cv2.putText() method
         cv2.rectangle(image, (x-10, y+10), (x+300, y-100), (0,0,0), cv2.FILLED)
         cv2.putText(image, letter, (x,y-25), font, 0.8, (255,255,255), 2, cv2.LINE_AA)
+        #cv2.putText(image, str(self.landmark_tensor), (x,y+500), font, 0.8, (255,255,255), 2, cv2.LINE_AA)
         # cv2.putText(image, letter_0, (x,y+100), font, 0.8, (255,255,255), 2, cv2.LINE_AA)
 
     def update_letter(self):
         self.stringOut_20 = "ID_20Coord( " + str(self.idSelX_20) + "," + str(self.idSelY_20) + " )"
         self.stringOut_0 = "ID_0Coord( " + str(self.idSelX_0) + "," + str(self.idSelY_0) + " )"
+
+    def estimate_letter(self):
+        pass
+
 
 def main():
     cap = cv2.VideoCapture(0)
