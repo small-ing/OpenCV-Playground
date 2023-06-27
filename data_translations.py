3f2a4bb25d5b783ac8f517eabde9bd208aebb8c9
Travis Smalling <main@smalling.dev>
2023-06-27 10:13:18 -0500

    updated scaling

--- data_translations.py
+++ data_translations.py
@@ -55,17 +55,24 @@ def collect_data(batch_size=24, offset=0):
     return empty_marks, empty_labels.astype(int)
 
 def normalize_data(data):
-    tensor_return = torch.zeros(data.shape)  
+    tensor_return = torch.zeros(data.shape)
     #print(tensor_return.shape)
     for i in range(len(data)): # ASL Letters iterated
+       # print(data[i].shape)
+        
         for j in range(1, 21): # 1-20 nodes iterated
+            width = data[i][:][0].max() - data[i][:][0].min()
+            height = data[i][:][1].max() - data[i][:][1].min()
+            #print(width)
+            #print(height)
+            width, height = int(width), int(height)
             zero_node = data[i][0] # saves 0 node before changes
             #print(zero_node)
             for k in range(2): # x/y iteration
                 if k == 0:
-                    tensor_return[i][j][k] = (zero_node[k] - data[i][j][k]) / 320
+                    tensor_return[i][j][k] = (zero_node[k] - data[i][j][k]) / width
                 if k == 1:
-                    tensor_return[i][j][k] = (zero_node[k] - data[i][j][k]) / 270
+                    tensor_return[i][j][k] = (zero_node[k] - data[i][j][k]) / height
             tensor_return[i][0][0] = 0 # reset zero node x
             tensor_return[i][0][1] = 0 # reset zero node y
     return tensor_return
@@ -75,13 +82,16 @@ def normalize_image_data(data):
     # print(tensor_return.shape)
     for i in range(len(data)): # ASL Letters iterated
         for j in range(1, 21): # 1-20 nodes iterated
+            width = data[i][:][0].max() - data[i][:][0].min()
+            height = data[i][:][1].max() - data[i][:][1].min()
+            width, height = int(width), int(height)
             zero_node = data[i][0] # saves 0 node before changes
             # print(zero_node)
             for k in range(2): # x/y iteration
                 if k == 0:
-                    tensor_return[i][j][k] = (zero_node[k] - data[i][j][k]) / 100
+                    tensor_return[i][j][k] = (zero_node[k] - data[i][j][k]) / width
                 if k == 1:
-                    tensor_return[i][j][k] = (zero_node[k] - data[i][j][k]) / 100
+                    tensor_return[i][j][k] = (zero_node[k] - data[i][j][k]) / height
             tensor_return[i][0][0] = 0 # reset zero node x
             tensor_return[i][0][1] = 0 # reset zero node y
     return tensor_return
@@ -227,6 +237,7 @@ def main():
     start_time = time.time()
     #print(start_time)
     train_data, train_labels = collect_data(24000)
+    test_data, test_labels = collect_data(2400, 21600)
     print("Collected JSON Data")
     bit_time = time.time()
     print("Collecting Image Data...")
@@ -239,7 +250,6 @@ def main():
     train_more_labels = train_more_labels[:zero_index]
     train_more_labels = train_more_labels.long()
     train_more_data = train_more_data[:zero_index]
-    test_data, test_labels = collect_data(2400, 21600)
     test_more_data, test_more_labels = collect_test_files(train_more_data, train_more_labels)
     test_more_data = test_more_data
     
