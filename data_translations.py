201a1cc54b79a4db08d6aca259d22b7ba4bfd7f1
Travis Smalling <main@smalling.dev>
2023-06-27 20:35:24 -0500

    potential memory saver, cleaned up data_translations + CNN training
    might end up reverting, but I brought the collecting of images outside the file in the hope that the MediaPipe prompt where it creates a new Tensorflow lite pack every epoch doesn't happen.

--- data_translations.py
+++ data_translations.py
@@ -3,13 +3,15 @@ import os
 import numpy as np
 from PIL import Image
 import cv2
-from ASLWebsite.hand_tracking_module import handTracker, CNN
+from ASLWebsite.hand_tracking_module import CNN
 import random
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.init
 import time
+import image_parser
+from image_parser import collect_train_files
 from alive_progress import alive_bar
 
 
@@ -17,7 +19,6 @@ from alive_progress import alive_bar
 landmark_map = [None, 0, None, 5, 6, 7, 8, None, 9, 10, 11, 12, None, 17, 18, 19, 20, None, 13, 14, 15, 16, 1, 2, 3, 4]
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-tracker = handTracker()
 alphabet = "abcdefghiklmnopqrstuvwxy"
 
 def collect_data(batch_size=24, offset=0):
@@ -86,42 +87,6 @@ def normalize_image_data(data):
             tensor_return[i][0][0] = 0 # reset zero node x
             tensor_return[i][0][1] = 0 # reset zero node y
     return tensor_return
-
-def collect_train_files():
-    landmarks = torch.zeros(80000, 21, 2)
-    labels = torch.zeros(87000)
-    j = 0
-    work = 0
-    errors = 0
-    for i in "ABCDEFGHIKLMNOPQRSTUVWXY":
-        #print("    Current Letter is " + i)
-        files = os.listdir("../../../Downloads/asl_images/asl_alphabet_train/asl_alphabet_train" + "/" + i)
-        with alive_bar(len(files), title=i) as bar:
-            for file_name in files:
-                with Image.open("../../../Downloads/asl_images/asl_alphabet_train/asl_alphabet_train" + "/" + i + "/" + file_name) as fileObject:
-                    fileObject = cv2.cvtColor(np.array(fileObject), cv2.COLOR_BGR2RGB)
-                    tracker.hands_finder(fileObject, False)
-                    hand_landmarks = tracker.results.multi_hand_landmarks
-                    if hand_landmarks is not None:
-                        hand_landmarks = hand_landmarks[0]
-                        landmarks[j] = torch.tensor([[lm.x, lm.y] for lm in hand_landmarks.landmark], dtype=torch.float32)
-                        labels[j] = ord(i) - ord("A") + 1
-                    else:
-                        fileObject = cv2.cvtColor(np.array(fileObject), cv2.COLOR_BGR2RGB)
-                        tracker.hands_finder(fileObject, False)
-                        hand_landmarks = tracker.results.multi_hand_landmarks
-                        if hand_landmarks is not None:
-                            hand_landmarks = hand_landmarks[0]
-                            landmarks[j] = torch.tensor([[lm.x, lm.y] for lm in hand_landmarks.landmark], dtype=torch.float32)
-                            labels[j] = ord(i) - ord("A") + 1
-                            work += 1
-                        else:
-                            errors += 1
-                bar()
-            # print("iteration: " + str(j), "errors: " + str(errors))
-            j += 1
-    print("retrying helped with " + str(work) + " images")
-    return landmarks, labels, errors
         
 def collect_test_files(train_landmarks, train_labels, num_files=100):
     landmarks = torch.zeros(num_files, 1, 21, 2)
@@ -146,7 +111,6 @@ class ImageDataset(torch.utils.data.Dataset):
 
 def train_model(model, train_loader, loss_fn, optimizer, epochs, test_images, test_labels):
     should_save = False
-    bar_it = 0
     for i in range(epochs):
         with alive_bar(len(train_loader)) as bar:
             for img, label in train_loader:
@@ -159,9 +123,7 @@ def train_model(model, train_loader, loss_fn, optimizer, epochs, test_images, te
                 optimizer.zero_grad()    
                 loss.backward()
                 optimizer.step()
-                bar_it += 1
-                if bar_it % 2 == 0:
-                    bar()
+                bar()
             test_images = test_images.to(torch.float).to(device)
             pred = model(test_images)
             digit = torch.argmax(pred, dim=1)
---
+++ image_parser.py
@@ -0,0 +1,45 @@
+import os
+import cv2
+import torch
+from alive_progress import alive_bar
+from PIL import Image
+import numpy as np
+from ASLWebsite.hand_tracking_module import handTracker, CNN
+
+tracker = handTracker()
+
+def collect_train_files():
+    landmarks = torch.zeros(80000, 21, 2)
+    labels = torch.zeros(87000)
+    j = 0
+    work = 0
+    errors = 0
+    for i in "ABCDEFGHIKLMNOPQRSTUVWXY":
+        #print("    Current Letter is " + i)
+        files = os.listdir("../../../Downloads/asl_images/asl_alphabet_train/asl_alphabet_train" + "/" + i)
+        with alive_bar(len(files), title=i) as bar:
+            for file_name in files:
+                with Image.open("../../../Downloads/asl_images/asl_alphabet_train/asl_alphabet_train" + "/" + i + "/" + file_name) as fileObject:
+                    fileObject = cv2.cvtColor(np.array(fileObject), cv2.COLOR_BGR2RGB)
+                    tracker.hands_finder(fileObject, False)
+                    hand_landmarks = tracker.results.multi_hand_landmarks
+                    if hand_landmarks is not None:
+                        hand_landmarks = hand_landmarks[0]
+                        landmarks[j] = torch.tensor([[lm.x, lm.y] for lm in hand_landmarks.landmark], dtype=torch.float32)
+                        labels[j] = ord(i) - ord("A") + 1
+                    else:
+                        fileObject = cv2.cvtColor(np.array(fileObject), cv2.COLOR_BGR2RGB)
+                        tracker.hands_finder(fileObject, False)
+                        hand_landmarks = tracker.results.multi_hand_landmarks
+                        if hand_landmarks is not None:
+                            hand_landmarks = hand_landmarks[0]
+                            landmarks[j] = torch.tensor([[lm.x, lm.y] for lm in hand_landmarks.landmark], dtype=torch.float32)
+                            labels[j] = ord(i) - ord("A") + 1
+                            work += 1
+                        else:
+                            errors += 1
+                bar()
+            # print("iteration: " + str(j), "errors: " + str(errors))
+            j += 1
+    print("retrying helped with " + str(work) + " images")
+    return landmarks, labels, errors
